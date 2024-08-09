e2248c0cd55a19df631392e878581125536085d5
Sergey Litvinov <slitvinov@gmail.com>
2024-08-09 09:57:23 -0400

    up

--- ComputeForces.cpp
+++ ComputeForces.cpp
@@ -1,12 +1,3 @@
-//
-//  CubismUP_2D
-//  Copyright (c) 2021 CSE-Lab, ETH Zurich, Switzerland.
-//  Distributed under the terms of the MIT license.
-//
-
-
-//#include <cstdio>
-//#include <iostream>
 #include <sstream>
 #include <string>
 #include <fstream>
