010bd939a5f35da4afbee1c143f1e7c65e13cbec
Sergey Litvinov <slitvinov@gmail.com>
2024-08-12 14:03:08 +0200

    up

--- main.cpp
+++ main.cpp
@@ -5981,7 +5981,6 @@ public:
   Shape(cubism::CommandlineParser &p, Real C[2]);
   virtual ~Shape();
   virtual Real getCharLength() const = 0;
-  virtual Real getCharMass() const;
   virtual Real getMaxVel() const;
   virtual void create(const std::vector<cubism::BlockInfo> &vInfo) = 0;
   virtual void finalize(){};
@@ -6119,7 +6118,6 @@ static void dump(Real time, ScalarGrid *grid, char *path) {
 }
 
 static constexpr Real EPS = std::numeric_limits<Real>::epsilon();
-Real Shape::getCharMass() const { return 0; }
 Real Shape::getMaxVel() const { return std::sqrt(u * u + v * v); }
 void Shape::updateVelocity(Real dt) {
   double A[3][3] = {{(double)penalM, (double)0, (double)-penalDY},
