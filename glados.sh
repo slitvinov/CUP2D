2ce82701b7be29fead423d28a703c96cdaa2b9c2
Sergey Litvinov <slitvinov@gmail.com>
2024-08-12 11:11:09 +0200

    up

--- glados.sh
+++ glados.sh
@@ -4,6 +4,6 @@ cd /scratch/lisergey/CUP2D &&
    git pull &&
    module load mpi &&
    make "NVCC =/usr/local/cuda-12.5/bin/nvcc -ccbin=mpic++" -j &&
-   mpiexec -n 2 run.sh
+   sh run.sh
 '
 scp -avz glados@/scratch/lisergey/CUP2D/vort* .
