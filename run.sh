600f751fdb1ca4a18546848fd320a724b26714ed
Sergey Litvinov <slitvinov@gmail.com>
2024-08-17 12:51:12 +0200

    rm unused bFixed

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 ./main -bpdx 2 -bpdy 1 -levelMax 8 -levelStart 5 -Rtol 2 -Ctol 1 -extent 4 -CFL 0.5 -poissonTol 1e-3 -poissonTolRel 1e-2 -maxPoissonRestarts 0 -bAdaptChiGradient 0 -tdump 0.1 -nu 0.00004 -tend 1.0 -shapes '
-       	       	 stefanfish L=0.2 T=1 xpos=1.8 ypos=0.2 bFixed=0 pidpos=0 pid=0
-		 stefanfish L=0.2 T=1 xpos=1.6 ypos=0.8 bFixed=0 pidpos=0 pid=0
+       	       	 stefanfish L=0.2 T=1 xpos=1.8 ypos=0.2
+		 stefanfish L=0.2 T=1 xpos=1.6 ypos=0.8
 '
