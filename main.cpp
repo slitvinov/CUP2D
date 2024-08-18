21c64ecfe4508c5dabc898bbed6f11ff258fa5dc
Sergey Litvinov <slitvinov@gmail.com>
2024-08-18 13:03:11 +0200

    up

--- main.cpp
+++ main.cpp
@@ -709,14 +709,13 @@ template <typename Block, typename ElementType> struct Grid {
   void dealloc_many(const std::vector<long long> &dealloc_IDs) {
     for (size_t j = 0; j < m_vInfo.size(); j++)
       m_vInfo[j].changed2 = false;
-    std::allocator<Block> alloc;
     for (size_t i = 0; i < dealloc_IDs.size(); i++)
       for (size_t j = 0; j < m_vInfo.size(); j++) {
         if (m_vInfo[j].blockID_2 == dealloc_IDs[i]) {
           const int m = m_vInfo[j].level;
           const long long n = m_vInfo[j].Z;
           m_vInfo[j].changed2 = true;
-          alloc.deallocate((Block *)getBlockInfoAll(m, n).ptrBlock, 1);
+	  delete [](Block *)getBlockInfoAll(m, n).ptrBlock;
           break;
         }
       }
