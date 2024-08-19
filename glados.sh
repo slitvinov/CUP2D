d92cc409994fa5c47fb93b8528f6fbc8c921f0eb
Sergey Litvinov <slitvinov@gmail.com>
2024-08-19 22:06:14 +0200

    rm more junk

--- glados.sh
+++ glados.sh
@@ -3,7 +3,7 @@ rm -rf /scratch/lisergey/CUP2D &&
    git clone git@github.com:slitvinov/CUP2D /scratch/lisergey/CUP2D &&
    cd /scratch/lisergey/CUP2D
    module load mpi &&
-   make "NVCC =/usr/local/cuda-12.5/bin/nvcc -ccbin=mpic++" "CXXFLAGS = -coverage -Og -g3" "LDFLAGS = -Xcompiler -coverage" -j
+   make "NVCC =/usr/local/cuda-12.5/bin/nvcc -ccbin=mpic++" "CXXFLAGS = -coverage -Og -g3" "LDFLAGS = -Xcompiler -coverage" "OPENMPFLAGS = " -j
    mpiexec -n 2 sh ~/run.sh &&
    python -m gcovr --html-details cover.html
 '
