d76fdf5d27f15490a98a0d1b33076e113812ceb7
Sergey Litvinov <slitvinov@gmail.com>
2024-08-21 20:26:38 +0200

    up

--- glados.sh
+++ glados.sh
@@ -4,6 +4,6 @@ rm -rf /scratch/lisergey/CUP2D &&
    cd /scratch/lisergey/CUP2D
    module load mpi &&
    make "NVCC =/usr/local/cuda-12.5/bin/nvcc -ccbin=mpic++" "CXXFLAGS = -O3" "LDFLAGS = -Xcompiler -coverage" "OPENMPFLAGS = " -j
-   mpiexec -n 2 sh ~/run.sh &&
+   mpiexec -n 2 sh ~/run.sh
 '
 rsync -avz glados:/scratch/lisergey/CUP2D/vort* .
