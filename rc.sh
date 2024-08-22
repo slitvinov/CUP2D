34ab95d725d30bf2d246ad7ca8dd68d15771b4b8
Sergey Litvinov <slitvinov@gmail.com>
2024-08-22 02:44:25 +0200

    fix

--- rc.sh
+++ rc.sh
@@ -1,13 +1,11 @@
 d=/n/holyscratch01/koumoutsakos_lab/slitvinov/CUP2D
 ssh rc '
 . /etc/profile
-module load gcc openmpi cuda &&
-set -x
 d='$d' 
 rm -rf "${d?not set}" &&
    git clone git@github.com:slitvinov/CUP2D "$d" &&
    cd "$d"
-   module load gcc openmpi cuda python &&
+   module load gcc openmpi cuda &&
    make -j "CXXFLAGS = -Wno-deprecated-declarations -Og -g3" -j &&
    OMP_NUM_THREADS=4 srun --mpi=pmix -l -p seas_gpu -c 1 -n 2 -N 1 --gpus 1 --mem 1Gb -t 15 sh -x run.sh
 ' &&
