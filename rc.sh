3ed7d0eeefb47b691a17e7590cce081f7831963e
Sergey Litvinov <slitvinov@gmail.com>
2024-08-23 02:34:31 +0200

    back to code coverage

--- rc.sh
+++ rc.sh
@@ -7,8 +7,10 @@ rm -rf "${d?not set}" &&
    cd "$d" &&
    git checkout '${1-HEAD}' &&
    module load gcc openmpi cuda python &&
-   make -j "CXXFLAGS = -Wno-deprecated-declarations -Og -g3" -j &&
+   # make -j "CXXFLAGS = -Wno-deprecated-declarations -Og -g3" -j &&
+   make -j "CXXFLAGS = -coverage -Og -g3" "LDFLAGS = -Xcompiler -coverage" "OPENMPFLAGS = "
    OMP_NUM_THREADS=4 srun --mpi=pmix -p seas_gpu -c 1 -n 2 -N 1 --gpus 1 --mem 2Gb -t 15 sh -x run.sh &&
    ls vort.*.xdmf2 | xargs -n 1 -P `nproc --all` ./post.py
+   python -m gcovr --html-details cover.html
 ' &&
-rsync -avz "rc:$d"/vort* .
+rsync -avz "rc:$d"/vort* "rc:$d"/cover* .
