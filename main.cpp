7da4dc8cbbdfe533882d894eca466ec2bc55662a
Sergey Litvinov <slitvinov@gmail.com>
2024-08-29 21:54:09 +0200

    up

--- main.cpp
+++ main.cpp
@@ -3397,7 +3397,7 @@ template <typename Element> struct LoadBalancer {
   const int dim;
   struct MPI_Block {
     long long mn[2];
-    Real data[max_dim * _BS_ * _BS_];
+    Real data[sizeof(BlockType) / sizeof(Real)];
     void prepare1(const BlockInfo &info) {
       mn[0] = info.level;
       mn[1] = info.Z;
