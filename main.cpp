488ffe22195c4fe7f1a5f0166b376ee4f5ff0cd1
Sergey Litvinov <slitvinov@gmail.com>
2024-08-30 22:05:08 +0200

    i

--- main.cpp
+++ main.cpp
@@ -2891,38 +2891,33 @@ template <typename Element> struct BlockLab {
       if (b_ptr == nullptr)
         continue;
       BlockType &b = *(BlockType *)b_ptr;
-      const int my_ix =
+      const int i =
           abs(code[0]) * (s[0] - start[0]) +
           (1 - abs(code[0])) * (s[0] - start[0] + (B % 2) * (e[0] - s[0]) / 2);
       const int XX =
           s[0] - code[0] * _BS_ + std::min(0, code[0]) * (e[0] - s[0]);
       for (int iz = s[2]; iz < e[2]; iz += 2) {
-        const int my_izx = my_ix;
         for (int iy = s[1]; iy < e[1] - mod; iy += 4 * yStep) {
           Element *ptrDest0 =
-              &m[my_izx +
-                 (abs(code[1]) * (iy + 0 * yStep - start[1]) +
-                  (1 - abs(code[1])) * ((iy + 0 * yStep) / 2 - start[1] +
-                                        aux * (e[1] - s[1]) / 2)) *
-                     nm[0]];
+              &m[i + (abs(code[1]) * (iy + 0 * yStep - start[1]) +
+                      (1 - abs(code[1])) * ((iy + 0 * yStep) / 2 - start[1] +
+                                            aux * (e[1] - s[1]) / 2)) *
+                         nm[0]];
           Element *ptrDest1 =
-              &m[my_izx +
-                 (abs(code[1]) * (iy + 1 * yStep - start[1]) +
-                  (1 - abs(code[1])) * ((iy + 1 * yStep) / 2 - start[1] +
-                                        aux * (e[1] - s[1]) / 2)) *
-                     nm[0]];
+              &m[i + (abs(code[1]) * (iy + 1 * yStep - start[1]) +
+                      (1 - abs(code[1])) * ((iy + 1 * yStep) / 2 - start[1] +
+                                            aux * (e[1] - s[1]) / 2)) *
+                         nm[0]];
           Element *ptrDest2 =
-              &m[my_izx +
-                 (abs(code[1]) * (iy + 2 * yStep - start[1]) +
-                  (1 - abs(code[1])) * ((iy + 2 * yStep) / 2 - start[1] +
-                                        aux * (e[1] - s[1]) / 2)) *
-                     nm[0]];
+              &m[i + (abs(code[1]) * (iy + 2 * yStep - start[1]) +
+                      (1 - abs(code[1])) * ((iy + 2 * yStep) / 2 - start[1] +
+                                            aux * (e[1] - s[1]) / 2)) *
+                         nm[0]];
           Element *ptrDest3 =
-              &m[my_izx +
-                 (abs(code[1]) * (iy + 3 * yStep - start[1]) +
-                  (1 - abs(code[1])) * ((iy + 3 * yStep) / 2 - start[1] +
-                                        aux * (e[1] - s[1]) / 2)) *
-                     nm[0]];
+              &m[i + (abs(code[1]) * (iy + 3 * yStep - start[1]) +
+                      (1 - abs(code[1])) * ((iy + 3 * yStep) / 2 - start[1] +
+                                            aux * (e[1] - s[1]) / 2)) *
+                         nm[0]];
           int YY0 = (abs(code[1]) == 1)
                         ? 2 * (iy + 0 * yStep - code[1] * _BS_) +
                               std::min(0, code[1]) * _BS_
@@ -2965,12 +2960,11 @@ template <typename Element> struct BlockLab {
           }
         }
         for (int iy = e[1] - mod; iy < e[1]; iy += yStep) {
-          Element *ptrDest =
-              (Element *)&m[my_izx +
-                            (abs(code[1]) * (iy - start[1]) +
-                             (1 - abs(code[1])) * (iy / 2 - start[1] +
-                                                   aux * (e[1] - s[1]) / 2)) *
-                                nm[0]];
+          Element *ptrDest = (Element *)&m[i + (abs(code[1]) * (iy - start[1]) +
+                                                (1 - abs(code[1])) *
+                                                    (iy / 2 - start[1] +
+                                                     aux * (e[1] - s[1]) / 2)) *
+                                                   nm[0]];
           int YY = (abs(code[1]) == 1)
                        ? 2 * (iy - code[1] * _BS_) + std::min(0, code[1]) * _BS_
                        : iy;
