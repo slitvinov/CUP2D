ff225a5cff0d6d0aebe4d96d410d1ce04d054d35
Sergey Litvinov <slitvinov@gmail.com>
2024-08-30 11:41:48 +0200

    up

--- main.cpp
+++ main.cpp
@@ -3787,7 +3787,7 @@ struct Adaptation {
           for (int J = 0; J < 2; J++)
             for (int I = 0; I < 2; I++) {
 	      void *bb = Blocks[J * 2 + I];
-              BlockType &b = (BlockType*)bb;
+              BlockType &b = *((BlockType*)bb);
               memset(&b, 0, dim * _BS_ * _BS_ * sizeof(Real));
               for (int j = 0; j < _BS_; j += 2)
                 for (int i = 0; i < _BS_; i += 2) {
