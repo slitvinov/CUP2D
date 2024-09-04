bc60112e74f576d5cfafe02afe91486ff78b15d4
Sergey Litvinov <slitvinov@gmail.com>
2024-09-04 07:53:20 +0300

    Real *

--- main.cpp
+++ main.cpp
@@ -6727,8 +6727,8 @@ struct pressure_rhs1 {
   StencilInfo stencil{-1, -1, 2, 2, false};
   void operator()(ScalarLab &lab, const BlockInfo &info) const {
     Real *um = (Real *)lab.m;
+    Real *TMP = (Real *)var.tmp->infos[info.id].block;
     int nm = _BS_ + stencil.ex - stencil.sx - 1;
-    ScalarBlock &TMP = *(ScalarBlock *)var.tmp->infos[info.id].block;
     for (int iy = 0; iy < _BS_; ++iy)
       for (int ix = 0; ix < _BS_; ++ix) {
         int ip0 = ix - stencil.sx;
@@ -6742,7 +6742,7 @@ struct pressure_rhs1 {
         Real *l2 = um + nm * jp0 + ip1;
         Real *l3 = um + nm * jm1 + ip0;
         Real *l4 = um + nm * jp1 + ip0;
-        TMP[iy][ix] -= *l1 + *l2 + *l3 + *l4 - 4 * (*l0);
+        TMP[_BS_ * iy + ix] -= *l1 + *l2 + *l3 + *l4 - 4 * (*l0);
       }
     BlockCase *tempCase = (BlockCase *)(var.tmp->infos[info.id].auxiliary);
     Real *faceXm = nullptr;
