5b874b6c7dc206c120a27853dd0d757c92a38cb0
Sergey Litvinov <slitvinov@gmail.com>
2024-09-05 18:47:07 +0200

    -n 4

--- rc.sh
+++ rc.sh
@@ -9,7 +9,7 @@ rm -rf "${d?not set}" &&
    module load gcc openmpi cuda python &&
    # make -j "CXXFLAGS = -Wno-deprecated-declarations -Og -g3" -j &&
    make -j "CXXFLAGS = -coverage -Og -g3" "LDFLAGS = -Xcompiler -coverage" "OPENMPFLAGS = " &&
-   OMP_NUM_THREADS=4 srun --x11 --mpi=pmix -p seas_gpu -c 1 -n 2 -N 1 --gpus 1 --mem 2Gb -t 30 sh -x run.sh &&
+   OMP_NUM_THREADS=4 srun --mpi=pmix -p seas_gpu -c 1 -n 4 -N 1 --gpus 1 --mem 2Gb -t 30 sh -x run.sh &&
    # ls vort.*.xdmf2 | xargs -n 1 -P `nproc --all` ./post.py &&
    python -m gcovr --html-details cover.html
 ' &&
