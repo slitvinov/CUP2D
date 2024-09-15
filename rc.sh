5f8737fc553afa3c34f71ec4c1d2a1b69904fb1d
Sergey Litvinov <slitvinov@gmail.com>
2024-09-15 18:25:49 +0200

    up

--- rc.sh
+++ rc.sh
@@ -9,8 +9,8 @@ rm -rf "${d?not set}" &&
    module load gcc/12.2.0-fasrc01 openmpi cuda python &&
    # make -j "CXXFLAGS = -Wno-deprecated-declarations -Og -g3" -j &&
    make -j "CXXFLAGS = -coverage -Og -g3" "LDFLAGS = -Xcompiler -coverage" "OPENMPFLAGS = " &&
-   OMP_NUM_THREADS=4 srun --mpi=pmix -p seas_gpu -c 1 -n 2 -N 1 --gpus 1 --mem 2Gb -t 30 sh -x run.sh
-   # ls vort.*.xdmf2 | xargs -n 1 -P `nproc --all` ./post.py &&
-   # python -m gcovr --html-details cover.html
+   OMP_NUM_THREADS=4 srun --mpi=pmix -p seas_gpu -c 1 -n 2 -N 1 --gpus 1 --mem 2Gb -t 30 sh -x run.sh &&
+   ls vort.*.xdmf2 | xargs -n 1 -P `nproc --all` ./post.py &&
+   python -m gcovr --html-details cover.html
 ' &&
 rsync -avz "rc:$d"/vort* .
