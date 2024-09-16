fbbb5a89a7a5c760acb6dbd0cc77dba094052795
Sergey Litvinov <slitvinov@gmail.com>
2024-09-16 11:59:37 +0200

    no defaults

--- run.sh
+++ run.sh
@@ -1,14 +1,22 @@
 : ${main=./main}
 $main \
+-AdaptSteps 20 \
 -bAdaptChiGradient 0 \
+-bMeanConstraint 0 \
 -bpdx 2 \
 -bpdy 1 \
 -CFL 0.5 \
 -Ctol 1 \
+-dlm 0 \
+-dt 0 \
 -extent 4 \
+-fdump 0 \
+-lambda 1e7 \
 -levelMax 8 \
 -levelStart 5 \
+-maxPoissonIterations 1000 \
 -maxPoissonRestarts 0 \
+-nsteps 0 \
 -nu 0.00004 \
 -poissonTol 1e-3 \
 -poissonTolRel 1e-2 \
