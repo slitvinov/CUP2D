01d5962be326ce365cc816b58d0ee060c42a7762
Sergey Litvinov <slitvinov@gmail.com>
2024-09-17 18:34:29 +0200

    up

--- main.cpp
+++ main.cpp
@@ -615,12 +615,12 @@ struct Range {
   const int ez = 1;
   bool needed{true};
   bool avg_down{true};
-  bool contains(Range &r) const {
-    if (avg_down != r.avg_down)
+  bool contains(Range *r) const {
+    if (avg_down != r->avg_down)
       return false;
     int V = (ey - sy) * (ex - sx);
-    int Vr = (r.ey - r.sy) * (r.ex - r.sx);
-    return sx <= r.sx && r.ex <= ex && sy <= r.sy && r.ey <= ey && Vr < V;
+    int Vr = (r->ey - r->sy) * (r->ex - r->sx);
+    return sx <= r->sx && r->ex <= ex && sy <= r->sy && r->ey <= ey && Vr < V;
   }
   void Remove(const Range &other) {
     size_t s = removedIndices.size();
@@ -689,7 +689,7 @@ static void needed0(std::vector<Range> compass[27], std::vector<int> &v) {
         if (me[j1].needed) {
           needme = true;
           for (size_t j2 = 0; j2 < me.size(); j2++)
-            if (me[j2].needed && me[j2].contains(me[j1])) {
+            if (me[j2].needed && me[j2].contains(&me[j1])) {
               me[j1].needed = false;
               me[j2].removedIndices.push_back(me[j1].index);
               me[j2].Remove(me[j1]);
@@ -716,7 +716,7 @@ static void needed0(std::vector<Range> compass[27], std::vector<int> &v) {
               if (o.needed)
                 for (size_t k1 = 0; k1 < me.size(); k1++) {
                   auto &m = me[k1];
-                  if (m.needed && m.contains(o)) {
+                  if (m.needed && m.contains(&o)) {
                     o.needed = false;
                     m.removedIndices.push_back(o.index);
                     m.Remove(o);
