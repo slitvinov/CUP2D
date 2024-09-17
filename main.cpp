152c466933edb3fb16cdf963943ff378a9d059f3
Sergey Litvinov <slitvinov@gmail.com>
2024-09-17 16:48:45 +0200

    trim

--- main.cpp
+++ main.cpp
@@ -6443,20 +6443,15 @@ struct pressure_rhs1 {
   }
 };
 struct FactoryFileLineParser : public CommandlineParser {
-  std::string &ltrim(std::string &s) {
-    s.erase(s.begin(),
-            std::find_if(s.begin(), s.end(),
-                         std::not1(std::ptr_fun<int, int>(std::isspace))));
-    return s;
+  std::string trim(std::string str) {
+    size_t i = 0, j = str.length();
+    while (i < j && isspace(str[i]))
+      i++;
+    while (j > i && isspace(str[j - 1])) {
+        j--;
+    }
+    return str.substr(i, j - i);
   }
-  std::string &rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(),
-                         std::not1(std::ptr_fun<int, int>(std::isspace)))
-                .base(),
-            s.end());
-    return s;
-  }
-  std::string &trim(std::string &s) { return ltrim(rtrim(s)); }
   FactoryFileLineParser(std::istringstream &is_line)
       : CommandlineParser(0, NULL) {
     std::string key, value;
