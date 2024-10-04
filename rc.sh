6163a3f20badc636c8a3f5df5c78651ae38f58d1
Sergey Litvinov <slitvinov@gmail.com>
2024-10-04 12:09:31 +0200

    up

--- rc.sh
+++ rc.sh
@@ -3,14 +3,14 @@ ssh rc '
 . /etc/profile
 d='$d' 
 rm -rf "${d?not set}" &&
-   git clone --depth 1 git@github.com:slitvinov/CUP2D "$d" &&
+   git clone git@github.com:slitvinov/CUP2D "$d" &&
    cd "$d" &&
    git checkout '${1-HEAD}' &&
    module load gcc/12.2.0-fasrc01 openmpi cuda python &&
    # make -j "CXXFLAGS = -Wno-deprecated-declarations -Og -g3" -j &&
    make -j "CXXFLAGS = -coverage -Og -g3" "LDFLAGS = -Xcompiler -coverage" "OPENMPFLAGS = " &&
-   OMP_NUM_THREADS=4 srun --mpi=pmix -p seas_gpu -c 1 -n 2 -N 1 --gpus 1 --mem 2Gb -t 30 sh -x run.sh
+   OMP_NUM_THREADS=4 srun --mpi=pmix -p seas_gpu -c 1 -n 2 -N 1 --gpus 1 --mem 2Gb -t 30 sh -x run.sh &&
    # ls vort.*.xdmf2 | xargs -n 1 -P `nproc --all` ./post.py &&
-   # python -m gcovr --html-details cover.html
+   PYTHONNOUSERSITE= python -m gcovr --html-details cover.html
 ' &&
 rsync -avz "rc:$d"/vort* .
