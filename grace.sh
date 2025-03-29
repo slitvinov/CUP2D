092db536b70cfa06901841a54aa47dbd973bedcc
Sergey Litvinov <slitvinov@gmail.com>
2025-03-29 10:27:17 +0100

    fix fill

--- grace.sh
+++ grace.sh
@@ -3,14 +3,13 @@ ssh grace2 '
 . /etc/profile
 d='$d'
 rm -rf "${d?not set}" &&
-   set -x &&
    git clone git@github.com:slitvinov/CUP2D "$d" &&
    cd "$d" &&
    git checkout '${1-HEAD}' &&
    MODULEPATH=/scratch/`whoami`/.grace/modulefiles:$MODULEPATH module load nvhpc/24.5 &&
-   # make -j "CXXFLAGS = -Wno-deprecated-declarations -Og -g3" -j &&
+   set -x &&
    make -j "CXXFLAGS = -O2 -g" "OPENMPFLAGS = " &&
-   OMP_NUM_THREADS=4 sh -x run.sh &&
+   sh -x run.sh &&
    ls vel.*.xdmf2 | xargs -n 1 -P `nproc --all` ./post.py
 ' &&
 rsync -avz "grace2:$d"/vel* .
