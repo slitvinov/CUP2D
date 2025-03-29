4181481644016fbba8400a5c7e767518fe9ad29c
Sergey Litvinov <slitvinov@gmail.com>
2025-03-29 23:01:52 +0100

    u

---
+++ tool/stat.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import numpy as np
+import sys
+import os
+import re
+import xml.etree.ElementTree
+
+for path in sys.argv[1:]:
+    dtype = np.dtype("float32")
+    path = re.sub("[.]xdmf2$", "", path)
+    path = re.sub("[.]attr\.raw$", "", path)
+    path = re.sub("[.]xyz\.raw$", "", path)
+    xdmf_path = path + ".xdmf2"
+    xyz_path = path + ".xyz.raw"
+    attr_path = path + ".attr.raw"
+    png_path = path + ".png"
+    root = xml.etree.ElementTree.parse(xdmf_path)
+    time = root.find("Domain/Grid/Time").get("Value")
+    xyz = np.memmap(xyz_path, dtype, "r")
+    ncell = xyz.size // (2 * 4)
+    assert ncell * 2 * 4 == xyz.size
+    attr = np.memmap(attr_path, dtype, "r")
+    attr = attr.reshape((ncell, -1))
+    print(np.mean(attr), np.std(attr), np.mean(xyz), np.std(attr))
