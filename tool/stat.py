65281426880c5e9acb9d5e3a7de8d7157c2029a3
Sergey Litvinov <slitvinov@gmail.com>
2025-04-01 23:58:11 +0200

    up

--- tool/stat.py
+++ tool/stat.py
@@ -12,7 +12,7 @@ for path in sys.argv[1:]:
     path = re.sub("[.]xyz\.raw$", "", path)
     xdmf_path = path + ".xdmf2"
     xyz_path = path + ".xyz.raw"
-    attr_path = path + ".attr.raw"
+    attr_path = path + ".chi.raw"
     root = xml.etree.ElementTree.parse(xdmf_path)
     time = root.find("Domain/Grid/Time").get("Value")
     xyz = np.memmap(xyz_path, dtype, "r")
