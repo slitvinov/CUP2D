937ee1697a93d469003b88ad20e3170ce3b85a61
Sergey Litvinov <slitvinov@gmail.com>
2025-04-02 00:01:49 +0200

    up

--- tool/stat.py
+++ tool/stat.py
@@ -6,7 +6,6 @@ import re
 import xml.etree.ElementTree
 
 for path in sys.argv[1:]:
-    dtype = np.dtype("float32")
     path = re.sub("[.]xdmf2$", "", path)
     path = re.sub("[.]attr\.raw$", "", path)
     path = re.sub("[.]xyz\.raw$", "", path)
@@ -15,10 +14,10 @@ for path in sys.argv[1:]:
     attr_path = path + ".chi.raw"
     root = xml.etree.ElementTree.parse(xdmf_path)
     time = root.find("Domain/Grid/Time").get("Value")
-    xyz = np.memmap(xyz_path, dtype, "r")
+    xyz = np.memmap(xyz_path, np.dtype("float32"), "r")
     ncell = xyz.size // (2 * 4)
     assert ncell * 2 * 4 == xyz.size
-    attr = np.memmap(attr_path, dtype, "r")
+    attr = np.memmap(attr_path, np.dtype("float64"), "r")
     attr = attr.reshape((ncell, -1))
     print(
         f"{np.mean(attr):+.2e} {np.std(attr):+.2e} {np.mean(xyz):+.2e} {np.std(attr):+.2e}"
