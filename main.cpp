d38c6f1c100d4e75112591632285d1d2dfcced65
Sergey Litvinov <slitvinov@gmail.com>
2025-04-02 07:50:32 +0200

    skip init

--- main.cpp
+++ main.cpp
@@ -4676,21 +4676,9 @@ int main(int argc, char **argv) {
   sim.space_curve->i_inverse[0].resize(1, -1);
   sim.space_curve->j_inverse[0].resize(1, -1);
   sim.space_curve->Zsave[0].resize(1, -1);
-  int c[2] = {0, 0};
-  long long index =
-      sim.space_curve->AxestoTranspose(c, 0);
-  assert(index == 0);
-  long long substract = 0;
-  for (long long h = 0; h < index; h++) {
-    int X[2] = {0, 0};
-    sim.space_curve->TransposetoAxes(h, X, 0);
-    if (X[0] >= 1 || X[1] >= 1)
-      substract++;
-  }
-  index -= substract;
-  sim.space_curve->i_inverse[0][index] = 0;
-  sim.space_curve->j_inverse[0][index] = 0;
-  sim.space_curve->Zsave[0][0] = index;
+  sim.space_curve->i_inverse[0][0] = 0;
+  sim.space_curve->j_inverse[0][0] = 0;
+  sim.space_curve->Zsave[0][0] = 0;
 
   std::string shapeArg = parser("shapes").asString();
   std::stringstream descriptors(shapeArg);
