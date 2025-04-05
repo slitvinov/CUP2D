d4b45d8fad995b6059f916fced1139097a9e2bab
Sergey Litvinov <slitvinov@gmail.com>
2025-04-05 12:56:51 +0200

    return halo_next

--- main.cpp
+++ main.cpp
@@ -434,10 +434,11 @@ static void FixDuplicates2(std::array<Range, 3 * 27> &AllStencils,
   *sz = 0;
 }
 
-static std::vector<Info *> &avail_next(
+static std::vector<Info *> *avail_next(
     std::unordered_map<std::string, HaloBlockGroup> &mapofHaloBlockGroups,
     std::unordered_map<int, MPI_Request *> &mapofrequests,
     std::vector<Info *> &dummy_vector) {
+  std::vector<Info *> *halo_next;
   for (;;) {
     bool all;
     all = true;
@@ -453,14 +454,20 @@ static std::vector<Info *> &avail_next(
         }
         if (flag == 1) {
           it.second.ready = true;
-          return it.second.myblocks;
+          halo_next = &it.second.myblocks;
+          goto done;
         }
       }
       all = all && it.second.ready;
     }
-    if (all)
-      return dummy_vector;
+    if (all) {
+      halo_next = &dummy_vector;
+      goto done;
+    }
   }
+
+done:
+  return halo_next;
 }
 
 struct SyncBuf {
@@ -2600,8 +2607,8 @@ static void computeA(Kernel &&kernel, Grid *g, int dim) {
     }
     while (done == false) {
 #pragma omp master
-      halo_next = &avail_next(Synch->mapofHaloBlockGroups, Synch->mapofrequests,
-                              Synch->dummy_vector);
+      halo_next = avail_next(Synch->mapofHaloBlockGroups, Synch->mapofrequests,
+                             Synch->dummy_vector);
 #pragma omp barrier
 #pragma omp for nowait
       for (std::size_t i = 0; i < halo_next->size(); ++i) {
