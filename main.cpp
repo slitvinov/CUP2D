e31f01e45090c47793fdb63da385d32010644a09
Sergey Litvinov <slitvinov@gmail.com>
2025-04-05 15:22:26 +0200

    sim.levels

--- main.cpp
+++ main.cpp
@@ -4742,9 +4742,10 @@ int main(int argc, char **argv) {
 
   sim.nblocks.resize(sim.size + 1);
   sim.nrows.resize(sim.size + 1);
-  sim.levels = {0};
-  for (int m = 1; m < sim.levelMax; m++)
-    sim.levels.push_back(sim.levels[m - 1] + 4 * (sim.levels[m - 1] + 1));
+  sim.levels.resize(sim.levelMax);
+  sim.levels[0] = 0;
+  for (int m = 0; m < sim.levelMax - 1; m++)
+    sim.levelMax[m + 1] = sim.levels[m] + (1 << 2 * m);
   long long total_blocks = 1LL << (2 * sim.levelStart);
   long long base = total_blocks / sim.size;
   long long rema = total_blocks % sim.size;
