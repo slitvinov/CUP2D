57d8c132eace751c4550d4e689be6ea9d1647ed0
Sergey Litvinov <slitvinov@gmail.com>
2025-04-06 16:28:33 +0200

    up

--- main.cpp
+++ main.cpp
@@ -2208,7 +2208,7 @@ public:
                       cy < 1 ? (cy < 0 ? offset[1] : 0) : _BS_ / 2};
           Real *dstbase =
               c + dim * (C[0] - offset[0] + (C[1] - offset[1]) * nc[0]);
-          Real *srcbase = &buf->recv_buffer[otherrank][unpack->offset] +
+          Real *srcbase = buf->recv_buffer[otherrank] + unpack->offset +
                           dim * (unpack->x + unpack->LX * unpack->y);
           int req = dim * (unpack->LX * unpack->ly + unpack->lx - unpack->LX);
           assert(unpack->lx == 0 || req + unpack->offset <=
