c37e352455ad1e288e0db427e8e177d9c80c7a11
Sergey Litvinov <slitvinov@gmail.com>
2025-04-06 17:34:33 +0200

    unpack->LX

--- main.cpp
+++ main.cpp
@@ -40,11 +40,13 @@ enum { max_dim = 2 };
               "  dim                    = %d\n"                                \
               "  buf                    = %g\n"                                \
               "  cond                   = %d\n",                               \
-              sim.rank, otherrank, otherrank,                                  \
-              buf->recv_buffer_size[otherrank], unpack->offset, req, dim,      \
+              "  unpack->lx                   = %d\n",                         \
+              "  unpack->LX                   = %d\n", sim.rank, otherrank,    \
+              otherrank, buf->recv_buffer_size[otherrank], unpack->offset,     \
+              req, dim,                                                        \
               buf->recv_buffer[otherrank][unpack->offset + dim * req - 1],     \
-              cond);                                                           \
-      /*      MPI_Abort(MPI_COMM_WORLD, 1);	*/			       \
+              cond, unpack->lx, unpack - LX);                                  \
+      /*      MPI_Abort(MPI_COMM_WORLD, 1);	*/                                 \
     }                                                                          \
   } while (0)
 
