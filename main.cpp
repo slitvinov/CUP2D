6fd7f8fd468885b18799a094dd856fe3f70e7ea2
Sergey Litvinov <slitvinov@gmail.com>
2025-04-06 12:05:11 +0200

    rm us

--- main.cpp
+++ main.cpp
@@ -1808,9 +1808,8 @@ public:
     int n = 1 << info->level;
     assert(m != NULL);
     Real *p = info->block;
-    Real *u = m;
     for (int iy = -start[1]; iy < -start[1] + _BS_; iy += 4) {
-      Real *q = u + dim * iy * nm[0] - dim * start[0];
+      Real *q = m + dim * iy * nm[0] - dim * start[0];
       memcpy(q, p, sizeof(Real) * dim * _BS_), q += dim * nm[0],
           p += dim * _BS_;
       memcpy(q, p, sizeof(Real) * dim * _BS_), q += dim * nm[0],
