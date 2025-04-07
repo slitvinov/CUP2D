f1eaf32726a88f23379544aecc05cbc8a2ca8d99
Sergey Litvinov <slitvinov@gmail.com>
2025-04-07 23:49:25 +0200

    up

--- main.cpp
+++ main.cpp
@@ -1491,12 +1491,11 @@ static void adapt() {
               continue;
             if (code[2] != 0)
               continue;
-            Info *infoNei = getf(&var.tmp->all, info->level,
+            Info *infoNei = getf0(&var.tmp->all, info->level,
                                  info->Znei[1 + code[0]][1 + code[1]]);
             if (Tree1(infoNei, &var.tmp->tree) >= 0 &&
                 infoNei->state == Refine) {
               info->state = Leave;
-              (getf(&var.tmp->all, info->level, info->Z))->state = Leave;
               break;
             }
           }
