836ecd15fb9bb633e241a053840a0047b21784cd
Sergey Litvinov <slitvinov@gmail.com>
2025-04-08 00:59:53 +0200

    clang-grace-toolchain-18

--- main.cpp
+++ main.cpp
@@ -619,28 +619,24 @@ public:
         continue;
       if (cy == yskip && yskin)
         continue;
-      if (!stencil.tensorial && !use_averages &&
-          abs(cx) + abs(cy) > 1)
+      if (!stencil.tensorial && !use_averages && abs(cx) + abs(cy) > 1)
         continue;
       int s[2] = {cx < 1 ? (cx < 0 ? start0[0] : 0) : _BS_,
                   cy < 1 ? (cy < 0 ? start0[1] : 0) : _BS_};
       int e[2] = {cx < 1 ? (cx < 0 ? 0 : _BS_) : _BS_ + end[0] - 1,
                   cy < 1 ? (cy < 0 ? 0 : _BS_) : _BS_ + end[1] - 1};
-      int sC[2] = {
-          cx < 1 ? (cx < 0 ? ((start0[0] - 1) / 2) : 0) : (_BS_ / 2),
-          cy < 1 ? (cy < 0 ? ((start0[1] - 1) / 2) : 0) : (_BS_ / 2)};
+      int sC[2] = {cx < 1 ? (cx < 0 ? ((start0[0] - 1) / 2) : 0) : (_BS_ / 2),
+                   cy < 1 ? (cy < 0 ? ((start0[1] - 1) / 2) : 0) : (_BS_ / 2)};
       int bytes = (e[0] - s[0]) * dim * sizeof(Real);
       if (!bytes)
         continue;
       if (use_averages) {
         for (int iy = s[1]; iy < e[1]; iy += 1) {
           int YY =
-              (iy - s[1] - std::min(0, cy) * ((e[1] - s[1]) % 2)) / 2 +
-              sC[1];
+              (iy - s[1] - std::min(0, cy) * ((e[1] - s[1]) % 2)) / 2 + sC[1];
           for (int ix = s[0]; ix < e[0]; ix += 1) {
             int XX =
-                (ix - s[0] - std::min(0, cx) * ((e[0] - s[0]) % 2)) / 2 +
-                sC[0];
+                (ix - s[0] - std::min(0, cx) * ((e[0] - s[0]) % 2)) / 2 + sC[0];
             Real *Test[3][3];
             for (int i = 0; i < 3; i++)
               for (int j = 0; j < 3; j++) {
@@ -652,28 +648,22 @@ public:
             for (int d = 0; d < dim; d++)
               TestInterp(
                   Test, m + dim * i1 + d,
-                  abs(ix - s[0] - std::min(0, cx) * ((e[0] - s[0]) % 2)) %
-                      2,
-                  abs(iy - s[1] - std::min(0, cy) * ((e[1] - s[1]) % 2)) %
-                      2);
+                  abs(ix - s[0] - std::min(0, cx) * ((e[0] - s[0]) % 2)) % 2,
+                  abs(iy - s[1] - std::min(0, cy) * ((e[1] - s[1]) % 2)) % 2);
           }
         }
       }
       if (abs(cx) + abs(cy) == 1) {
         for (int iy = s[1]; iy < e[1]; iy += 2) {
-          int YY =
-              (iy - s[1] - std::min(0, cy) * ((e[1] - s[1]) % 2)) / 2 +
-              sC[1] - offset[1];
-          int y =
-              abs(iy - s[1] - std::min(0, cy) * ((e[1] - s[1]) % 2)) % 2;
+          int YY = (iy - s[1] - std::min(0, cy) * ((e[1] - s[1]) % 2)) / 2 +
+                   sC[1] - offset[1];
+          int y = abs(iy - s[1] - std::min(0, cy) * ((e[1] - s[1]) % 2)) % 2;
           int iyp = (abs(iy) % 2 == 1) ? -1 : 1;
           double dy = 0.25 * (2 * y - 1);
           for (int ix = s[0]; ix < e[0]; ix += 2) {
-            int XX =
-                (ix - s[0] - std::min(0, cx) * ((e[0] - s[0]) % 2)) / 2 +
-                sC[0] - offset[0];
-            int x =
-                abs(ix - s[0] - std::min(0, cx) * ((e[0] - s[0]) % 2)) % 2;
+            int XX = (ix - s[0] - std::min(0, cx) * ((e[0] - s[0]) % 2)) / 2 +
+                     sC[0] - offset[0];
+            int x = abs(ix - s[0] - std::min(0, cx) * ((e[0] - s[0]) % 2)) % 2;
             int ixp = (abs(ix) % 2 == 1) ? -1 : 1;
             double dx = 0.25 * (2 * x - 1);
             if (ix < -2 || iy < -2 || ix > _BS_ + 1 || iy > _BS_ + 1)
@@ -771,10 +761,8 @@ public:
             int k10 = ix - start0[0] + 3 + nm[0] * (iy - start0[1]);
             int k11 = ix - start0[0] + nm[0] * (iy - start0[1] - 3);
             int k12 = ix - start0[0] + nm[0] * (iy - start0[1]);
-            int x =
-                abs(ix - s[0] - std::min(0, cx) * ((e[0] - s[0]) % 2)) % 2;
-            int y =
-                abs(iy - s[1] - std::min(0, cy) * ((e[1] - s[1]) % 2)) % 2;
+            int x = abs(ix - s[0] - std::min(0, cx) * ((e[0] - s[0]) % 2)) % 2;
+            int y = abs(iy - s[1] - std::min(0, cy) * ((e[1] - s[1]) % 2)) % 2;
             for (int d = 0; d < dim; d++) {
               Real *a = m + dim * k12 + d;
               if (cx == 0 && cy == 1) {
@@ -1482,14 +1470,14 @@ static void adapt() {
           for (int icode = 0; icode < 27; icode++) {
             if (icode == 1 * 1 + 3 * 1 + 9 * 1)
               continue;
-	    int cx = icode % 3 - 1;
-	    int cy = (icode / 3) % 3 - 1;
+            int cx = icode % 3 - 1;
+            int cy = (icode / 3) % 3 - 1;
             if (cx == xskip && xskin)
               continue;
             if (cy == yskip && yskin)
               continue;
-            Info *infoNei = getf(&var.tmp->all, info->level,
-                                 info->Znei[1 + cx][1 + cy]);
+            Info *infoNei =
+                getf(&var.tmp->all, info->level, info->Znei[1 + cx][1 + cy]);
             if (Tree1(infoNei, &var.tmp->tree) >= 0 &&
                 infoNei->state == Refine) {
               info->state = Leave;
