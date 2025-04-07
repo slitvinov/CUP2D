24cf6cd6b3c3b4fa5e85b90a59258a86c939ca33
Sergey Litvinov <slitvinov@gmail.com>
2025-04-07 13:58:27 +0200

    up

--- cuda.cu
+++ cuda.cu
@@ -631,12 +631,13 @@ void LocalSpMatDnVec::make(const std::vector<long long> &Nrows_xcumsum) {
   send_pack_idx_.resize(offset);
   std::vector<long long> recv_idx_list(halo_);
   for (size_t i(0); i < send_ranks_.size(); i++) {
-    MPI_Recv(&send_pack_idx_long[send_offset_[i]], send_sz_[i], MPI_LONG_LONG,
+    int n = send_sz_[i];
+    MPI_Recv(&send_pack_idx_long[send_offset_[i]], n, MPI_LONG_LONG,
              send_ranks_[i], 546, m_comm_, MPI_STATUS_IGNORE);
     std::copy(bd_recv_set_[recv_ranks_[i]].begin(),
               bd_recv_set_[recv_ranks_[i]].end(),
               &recv_idx_list[recv_offset_[i]]);
-    MPI_Send(&recv_idx_list[recv_offset_[i]], recv_sz_[i], MPI_LONG_LONG,
+    MPI_Send(&recv_idx_list[recv_offset_[i]], n, MPI_LONG_LONG,
              recv_ranks_[i], 546, m_comm_);
   }
   const long long shift = -Nrows_xcumsum[rank_];
