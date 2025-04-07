f5afb6bed839fcc5724cf8b42b36a08878b76e2c
Sergey Litvinov <slitvinov@gmail.com>
2025-04-07 13:44:18 +0200

    status

--- cuda.cu
+++ cuda.cu
@@ -637,14 +637,14 @@ void LocalSpMatDnVec::make(const std::vector<long long> &Nrows_xcumsum) {
   send_pack_idx_.resize(offset);
   for (size_t i(0); i < send_ranks_.size(); i++)
     MPI_Recv(&send_pack_idx_long[send_offset_[i]], send_sz_[i], MPI_LONG_LONG,
-	     send_ranks_[i], 546, m_comm_);
+	     send_ranks_[i], 546, m_comm_, MPI_STATUS_IGNORE);
   std::vector<long long> recv_idx_list(halo_);
   for (size_t i(0); i < recv_ranks_.size(); i++) {
     std::copy(bd_recv_set_[recv_ranks_[i]].begin(),
               bd_recv_set_[recv_ranks_[i]].end(),
               &recv_idx_list[recv_offset_[i]]);
     MPI_Send(&recv_idx_list[recv_offset_[i]], recv_sz_[i], MPI_LONG_LONG,
-	     recv_ranks_[i], 546, m_comm_);
+	     recv_ranks_[i], 546, m_comm_, MPI_STATUS_IGNORE);
   }
   const long long shift = -Nrows_xcumsum[rank_];
   loc_cooRowA_int_.resize(loc_nnz_);
@@ -662,7 +662,6 @@ void LocalSpMatDnVec::make(const std::vector<long long> &Nrows_xcumsum) {
     for (int i = 0; i < bd_nnz_; i++)
       bd_cooRowA_int_[i] = (int)(bd_cooRowA_long_[i] + shift);
   }
-  MPI_Waitall(send_ranks_.size(), recv_requests.data(), MPI_STATUS_IGNORE);
   std::unordered_map<long long, int> bd_reindex_map;
   bd_reindex_map.reserve(halo_);
   for (int i(0); i < halo_; i++)
