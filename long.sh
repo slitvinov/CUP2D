731e6373a0338b1d1717472aa99c7cb55b02a188
Sergey Litvinov <slitvinov@gmail.com>
2025-04-08 20:52:54 +0200

    minor

--- long.sh
+++ long.sh
@@ -1,14 +1,14 @@
 ${main=./main} \
 -AdaptSteps 20 \
 -CFL 0.5 \
--Ctol 1 \
+-Ctol 0.01 \
 -lambda 1e7 \
 -levelMax 9 \
 -levelStart 4 \
 -maxPoissonRestarts 0 \
 -nu 1e-4 \
 -poissonTol 1e-3 \
--poissonTolRel 1e-2 \
+-poissonTolRel 0 \
 -Rtol 0.1 \
 -tdump 0.25 \
 -tend 600 \
