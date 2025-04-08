43bdf5378adad67c07f2c6185d84c6f3fb096481
Sergey Litvinov <slitvinov@gmail.com>
2025-04-08 15:25:35 +0200

    up

--- tool/view.py
+++ tool/view.py
@@ -1,12 +1,16 @@
 import sys
+import re
 
 sys.argv.pop(0)
 if sys.argv:
     xdmf_path = sys.argv.pop(0)
-    png_path = sys.argv.pop(0)
+    if not sys.argv:
+        png_path = re.sub("[.]xdmf2$", "", xdmf_path) + ".png"
+    else:
+        png_path = sys.argv.pop(0)
 else:
-    xdmf_path = "vel.00000080.xdmf2"
-    png_path = "vel.00000080.png"
+    xdmf_path = "vel.000000002.xdmf2"
+    png_path = "vel.000000002.png"
 import paraview
 from paraview.simple import *
 
@@ -20,30 +24,21 @@ layout = CreateLayout()
 layout.AssignView(0, view)
 layout.SetSize(*view.ViewSize)
 xdmf = OpenDataFile(xdmf_path)
-disp = GetDisplayProperties(xdmf)
-tmpLUT = GetColorTransferFunction('vort')
-tmpLUT.RGBPoints = [
-    -1.0, 0.176471, 0.0, 0.294118, -0.87451, 0.272434, 0.095963, 0.444214,
-    -0.74902, 0.373395, 0.228912, 0.56932, -0.623529, 0.481661, 0.415917,
-    0.657901, -0.498039, 0.601922, 0.562937, 0.750481, -0.372549, 0.718493,
-    0.695886, 0.836986, -0.24705900000000003, 0.811995, 0.811534, 0.898501,
-    -0.12156900000000004, 0.894733, 0.8995, 0.940023, 0.00392156999999993,
-    0.969166, 0.966859, 0.963629, 0.12941200000000008, 0.98639, 0.910265,
-    0.803691, 0.25490199999999996, 0.995002, 0.835371, 0.624375,
-    0.38039200000000006, 0.992541, 0.736947, 0.420146, 0.5058820000000002,
-    0.931949, 0.609458, 0.224221, 0.631373, 0.85075, 0.483968, 0.069819,
-    0.7568630000000001, 0.740023, 0.380623, 0.035371, 0.8823530000000002,
-    0.617993, 0.29827, 0.026759, 1.0, 0.498039, 0.231373, 0.031373
-]
+threshold = Threshold(Input=xdmf)
+threshold.Scalars = 'CELLS', 'chi'
+threshold.UpperThreshold = 0.5
+disp = Show(threshold)
+tmpLUT = GetColorTransferFunction('tmp')
+tmpLUT.RGBPoints = [-1.0, 0.176471, 0.0, 0.294118, -0.87451, 0.272434, 0.095963, 0.444214, -0.74902, 0.373395, 0.228912, 0.56932, -0.623529, 0.481661, 0.415917, 0.657901, -0.498039, 0.601922, 0.562937, 0.750481, -0.372549, 0.718493, 0.695886, 0.836986, -0.24705900000000003, 0.811995, 0.811534, 0.898501, -0.12156900000000004, 0.894733, 0.8995, 0.940023, 0.00392156999999993, 0.969166, 0.966859, 0.963629, 0.12941200000000008, 0.98639, 0.910265, 0.803691, 0.25490199999999996, 0.995002, 0.835371, 0.624375, 0.38039200000000006, 0.992541, 0.736947, 0.420146, 0.5058820000000002, 0.931949, 0.609458, 0.224221, 0.631373, 0.85075, 0.483968, 0.069819, 0.7568630000000001, 0.740023, 0.380623, 0.035371, 0.8823530000000002, 0.617993, 0.29827, 0.026759, 1.0, 0.498039, 0.231373, 0.031373]
 disp.LookupTable = tmpLUT
 disp.Representation = 'Surface'
-ColorBy(disp, ('CELLS', 'vort', 'X'))
+disp.ColorArrayName = ['CELLS', 'tmp']
 
 view.CameraParallelProjection = 1
 view.UseColorPaletteForBackground = 0
 view.Background = 1, 1, 1
 
-bnd = xdmf.GetDataInformation().GetBounds()
+bnd = threshold.GetDataInformation().GetBounds()
 margin = 0
 cx = 0.5 * (bnd[0] + bnd[1])
 cy = 0.5 * (bnd[2] + bnd[3])
