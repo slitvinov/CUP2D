c3809ae7e2ae48925db92e23ec7786b8b6fa396e
Sergey Litvinov <slitvinov@gmail.com>
2025-04-08 19:50:12 +0200

    up

--- tool/blob.py
+++ tool/blob.py
@@ -1,6 +1,7 @@
 import jax
 import jax.numpy as jnp
 import struct
+import numpy as np
 
 x0, y0, s0 = 0.0, 0.0, 1.0
 x1, y1, s1 = 2.0, 1.0, 2.0
@@ -28,28 +29,31 @@ def sdf_ratio(xy):
 sdf_ratio_batch = jax.vmap(sdf_ratio)
 nr, np0 = 800, 800
 length = 2 * max(s0, s1)
-rmax = 3 * length
+rmax = 4 * length
 dr = rmax / nr
 dp = 2 * jnp.pi / (np0 - 1)
-i = jnp.arange(1, nr + 1) * dr
-j = jnp.arange(np0) * dp
-R, P = jnp.meshgrid(i, j, indexing="ij")
+r = jnp.arange(1, nr + 1) * dr
+p = jnp.arange(np0) * dp
+R, P = jnp.meshgrid(r, p, indexing="ij")
 x = R * jnp.cos(P)
 y = R * jnp.sin(P)
 xy = jnp.stack([x.ravel(), y.ravel()], axis=-1)
 sdf = sdf_ratio_batch(xy).reshape(x.shape)
 mask = sdf > 0
 weights = (R**2) * mask
-area = jnp.sum(weights) * dr * dp
-xc = jnp.sum(weights * x) * dr * dp / area
-yc = jnp.sum(weights * y) * dr * dp / area
-
-x_shift = x - xc
-y_shift = y - yc
+area = jnp.sum(weights)
+xc = jnp.sum(weights * x) / area
+yc = jnp.sum(weights * y) / area
+area *=  dr * dp
+scale = 1 / jnp.sqrt(area)
+x_shift = (x + xc) / scale
+y_shift = (y + yc) / scale
 xy_shift = jnp.stack([x_shift.ravel(), y_shift.ravel()], axis=-1)
-sdf_shifted = sdf_ratio_batch(xy_shift).reshape(x.shape)
+sdf_shifted = scale * sdf_ratio_batch(xy_shift).reshape(x.shape)
 with open("blob.raw", "wb") as f:
     f.write(b"SDF")
-    f.write(struct.pack("ffii", float(length), float(rmax), nr, np0))
-    for val in sdf_shifted.ravel():
-        f.write(struct.pack("f", float(val)))
+    f.write(struct.pack("ffii", float(length * scale), float(rmax * scale), nr, np0))
+    f.write(np.asarray(sdf_shifted, dtype=np.float32).tobytes())
+for xi, yi, si in zip(x.ravel(), y.ravel(), sdf_shifted.ravel()):
+    if si > 0:
+        print(xi, yi, si)
