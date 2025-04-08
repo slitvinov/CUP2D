cad8bc24c626438959ec1b8470283e6b148d583b
Sergey Litvinov <slitvinov@gmail.com>
2025-04-08 15:28:41 +0200

    up

---
+++ long.sh
@@ -0,0 +1,18 @@
+${main=./main} \
+-AdaptSteps 20 \
+-CFL 0.5 \
+-Ctol 1 \
+-lambda 1e7 \
+-levelMax 7 \
+-levelStart 4 \
+-maxPoissonRestarts 0 \
+-nu 1e-4 \
+-poissonTol 1e-3 \
+-poissonTolRel 1e-2 \
+-Rtol 0.1 \
+-tdump 0.25 \
+-tend 600 \
+-shapes '
+   scale=0.125 orientation=0 omega=-1 xcenter=0.3 ycenter=0.5 sdf=box.raw
+   scale=0.125 orientation=0 omega=1 xcenter=0.6 ycenter=0.5 sdf=box.raw
+'
