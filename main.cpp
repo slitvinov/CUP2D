e876b3a8e1ff7c619eb74fe4a71618d85579c99c
Sergey Litvinov <slitvinov@gmail.com>
2025-04-08 13:30:29 +0200

    use getf1

--- main.cpp
+++ main.cpp
@@ -2043,13 +2043,13 @@ struct Solver {
       row.mapColVal(nei_rank, nei_idx, 1.);
       row.mapColVal(sfc_idx, -1.);
     } else if (Tree1(rhsNei, &var.tmp->tree) == RefinedChildren) {
-      Info *rhsNei_c =
-          getf0(&var.tmp->all, rhs_info->level - 1, rhsNei->Zparent);
+      Info rhsNei_c =
+          getf1(&var.tmp->all, rhs_info->level - 1, rhsNei->Zparent);
       int ix_c = indexer->ix_c(rhs_info, ix);
       int iy_c = indexer->iy_c(rhs_info, iy);
       long long inward_idx = indexer->neiInward(rhs_info, ix, iy);
       double signTaylor = indexer->taylorSign(ix, iy);
-      interpolate(rhsNei_c, ix_c, iy_c, rhs_info, sfc_idx, inward_idx, 1.,
+      interpolate(&rhsNei_c, ix_c, iy_c, rhs_info, sfc_idx, inward_idx, 1.,
                   signTaylor, indexer, row);
       row.mapColVal(sfc_idx, -1.);
     } else if (Tree1(rhsNei, &var.tmp->tree) == CoarseNeighbour) {
