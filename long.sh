713edda775017b6b24a43890f7f591c7c3fe6aa0
Sergey Litvinov <slitvinov@gmail.com>
2025-04-08 21:17:33 +0200

    0.5

--- long.sh
+++ long.sh
@@ -13,6 +13,10 @@ ${main=./main} \
 -tdump 0.1 \
 -tend 600 \
 -shapes '
+scale=0.06 orientation=0 omega=-1.82 xcenter=0.5 ycenter=0.5 sdf=blob.raw
+'
+
+: '
    scale=0.06 orientation=0 omega=-1.82 xcenter=0.44 ycenter=0.38 sdf=blob.raw
    scale=0.06 orientation=0 omega=-1.92 xcenter=0.44 ycenter=0.53 sdf=blob.raw
    scale=0.06 orientation=0 omega=1.54 xcenter=0.44 ycenter=0.68 sdf=blob.raw
