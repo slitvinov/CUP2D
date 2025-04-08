dfe35b27ee2791f81e3416db57f51a3e71c02256
Sergey Litvinov <slitvinov@gmail.com>
2025-04-08 16:00:38 +0200

    restore mass

--- main.cpp
+++ main.cpp
@@ -990,6 +990,7 @@ struct Obstacle {
   Real udef[_BS_][_BS_][2];
   Real COM_x = 0;
   Real COM_y = 0;
+  Real Mass = 0;
   Obstacle() {
     std::fill(&dist[0][0], &dist[0][0] + _BS_ * _BS_, -1);
     memset(&chi[0][0], 0, sizeof(Real) * _BS_ * _BS_);
@@ -1161,6 +1162,7 @@ struct PutChiOnGrid {
       Obstacle &o = *oblock[info->id];
       o.COM_x = 0;
       o.COM_y = 0;
+      o.Mass = 0;
       Real *CHI = chiInfo[info->id]->block;
       Real *chi = (Real *)o.chi;
       Real *dist = (Real *)o.dist;
@@ -1198,6 +1200,7 @@ struct PutChiOnGrid {
             p[1] = info->origin[1] + info->h * (iy + 0.5);
             o.COM_x += chi[j] * h2 * (p[0] - shape->x);
             o.COM_y += chi[j] * h2 * (p[1] - shape->y);
+            o.Mass += chi[j] * h2;
           }
         }
     }
@@ -1276,6 +1279,7 @@ static void ongrid() {
     for (size_t i = 0; i < oblock.size(); i++) {
       if (oblock[i] == nullptr)
         continue;
+      com[0] += oblock[i]->Mass;
       com[1] += oblock[i]->COM_x;
       com[2] += oblock[i]->COM_y;
     }
