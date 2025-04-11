ef89af33e08b958ce68e8e3b6a145b309197f342
Sergey Litvinov <slitvinov@gmail.com>
2025-04-11 23:11:28 +0200

    rm count

--- main.cpp
+++ main.cpp
@@ -379,9 +379,7 @@ public:
         int ys = pattern->ys;
         int mod = ((e[1] - s[1]) / ys) % 4;
         assert(pattern);
-        int count = 0;
         for (int B = 0; B <= 3; B += pattern->Bstep) {
-          count++;
           int aux = (abs(cx) == 1) ? (B % 2) : (B / 2);
           int ix = 2 * xi + std::max(cx, 0) + cx +
                    (B % 2) * std::max(0, 1 - abs(cx));
