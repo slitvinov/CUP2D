ddfdbb2cf82ab5cdfd8785d5bd9ecdea6e46f5d2
Sergey Litvinov <slitvinov@gmail.com>
2025-04-12 22:42:54 +0200

    upd

--- main.cpp
+++ main.cpp
@@ -1711,22 +1711,25 @@ static void adapt() {
         }
       int offsetX[2] = {0, BS / 2};
       int offsetY[2] = {0, BS / 2};
-      for (int J = 0; J < 2; J++)
-        for (int I = 0; I < 2; I++) {
-          Real *b = Blocks[J * 2 + I]  + offset * BS * BS;;
-          for (int j = 0; j < BS; j += 2)
-            for (int i = 0; i < BS; i += 2) {
-              int i00 = BS * j + i;
-              int i01 = BS * (j + 1) + i;
-              int i10 = BS * j + i + 1;
-              int i11 = BS * (j + 1) + i + 1;
-              int o = BS * (j / 2 + offsetY[J]) + i / 2 + offsetX[I];
-              for (int d = 0; d < dim; d++)
-                Blocks[0][dim * o + d] = (b[dim * i00 + d] + b[dim * i01 + d] +
-                                          b[dim * i10 + d] + b[dim * i11 + d]) /
-                                         4;
-            }
-        }
+      for (size_t k = 0; k < sizeof var.F / sizeof *var.F; k++) {
+        for (int J = 0; J < 2; J++)
+          for (int I = 0; I < 2; I++) {
+            Real *b = Blocks[J * 2 + I] + offset * BS * BS;
+            for (int j = 0; j < BS; j += 2)
+              for (int i = 0; i < BS; i += 2) {
+                int i00 = BS * j + i;
+                int i01 = BS * (j + 1) + i;
+                int i10 = BS * j + i + 1;
+                int i11 = BS * (j + 1) + i + 1;
+                int o = BS * (j / 2 + offsetY[J]) + i / 2 + offsetX[I];
+                for (int d = 0; d < dim; d++)
+                  Blocks[0][dim * o + d] =
+                      (b[dim * i00 + d] + b[dim * i01 + d] + b[dim * i10 + d] +
+                       b[dim * i11 + d]) /
+                      4;
+              }
+          }
+      }
       long long np = forward(level - 1, info->index[0] / 2, info->index[1] / 2);
       Info *parent = getf0(&g->all, level - 1, np);
 #pragma omp critical
