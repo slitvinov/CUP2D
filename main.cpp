243f870c1f15df0e859c07a40fecf9097d909443
Sergey Litvinov <slitvinov@gmail.com>
2025-04-12 12:11:27 +0200

    rename args in D1

--- main.cpp
+++ main.cpp
@@ -1999,22 +1999,22 @@ struct Solver {
   YminIndexer YminCell;
   YmaxIndexer YmaxCell;
   std::array<const EdgeCellIndexer *, 4> edgeIndexers;
-  std::array<std::pair<long long, double>, 3>
-  static D1(const Info *info, const EdgeCellIndexer *indexer, int ix, int iy) {
-    if (indexer->isBD(ix, iy))
-      return {{{indexer->Nei(info, ix, iy, -2), 1. / 8.},
-               {indexer->Nei(info, ix, iy, -1), -1. / 2.},
-               {This(info, ix, iy), 3. / 8.}}};
-    else if (indexer->isFD(ix, iy))
-      return {{{indexer->Nei(info, ix, iy, 2), -1. / 8.},
-               {indexer->Nei(info, ix, iy, 1), 1. / 2.},
-               {This(info, ix, iy), -3. / 8.}}};
-    return {{{indexer->Nei(info, ix, iy, -1), -1. / 8.},
-             {indexer->Nei(info, ix, iy, 1), 1. / 8.},
-             {This(info, ix, iy), 0.}}};
+  std::array<std::pair<long long, double>, 3> static D1(
+      const Info *info, const EdgeCellIndexer *indexer, int ix_c, int iy_c) {
+    if (indexer->isBD(ix_c, iy_c))
+      return {{{indexer->Nei(info, ix_c, iy_c, -2), 1. / 8.},
+               {indexer->Nei(info, ix_c, iy_c, -1), -1. / 2.},
+               {This(info, ix_c, iy_c), 3. / 8.}}};
+    else if (indexer->isFD(ix_c, iy_c))
+      return {{{indexer->Nei(info, ix_c, iy_c, 2), -1. / 8.},
+               {indexer->Nei(info, ix_c, iy_c, 1), 1. / 2.},
+               {This(info, ix_c, iy_c), -3. / 8.}}};
+    return {{{indexer->Nei(info, ix_c, iy_c, -1), -1. / 8.},
+             {indexer->Nei(info, ix_c, iy_c, 1), 1. / 8.},
+             {This(info, ix_c, iy_c), 0.}}};
   }
-  std::array<std::pair<long long, double>, 3>
-  static D2(const Info *info, const EdgeCellIndexer *indexer, int ix, int iy) {
+  std::array<std::pair<long long, double>, 3> static D2(
+      const Info *info, const EdgeCellIndexer *indexer, int ix, int iy) {
     if (indexer->isBD(ix, iy))
       return {{{indexer->Nei(info, ix, iy, -2), 1. / 32.},
                {indexer->Nei(info, ix, iy, -1), -1. / 16.},
