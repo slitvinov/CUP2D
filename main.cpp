5fd7ff8f64cd26de4ed52521035d0bc6149f03f5
Sergey Litvinov <slitvinov@gmail.com>
2025-04-12 12:13:01 +0200

    info

--- main.cpp
+++ main.cpp
@@ -2000,18 +2000,18 @@ struct Solver {
   YmaxIndexer YmaxCell;
   std::array<const EdgeCellIndexer *, 4> edgeIndexers;
   std::array<std::pair<long long, double>, 3> static D1(
-      const Info *info, const EdgeCellIndexer *indexer, int ix_c, int iy_c) {
+      const Info *info_c, const EdgeCellIndexer *indexer, int ix_c, int iy_c) {
     if (indexer->isBD(ix_c, iy_c))
-      return {{{indexer->Nei(info, ix_c, iy_c, -2), 1. / 8.},
-               {indexer->Nei(info, ix_c, iy_c, -1), -1. / 2.},
-               {This(info, ix_c, iy_c), 3. / 8.}}};
+      return {{{indexer->Nei(info_c, ix_c, iy_c, -2), 1. / 8.},
+               {indexer->Nei(info_c, ix_c, iy_c, -1), -1. / 2.},
+               {This(info_c, ix_c, iy_c), 3. / 8.}}};
     else if (indexer->isFD(ix_c, iy_c))
-      return {{{indexer->Nei(info, ix_c, iy_c, 2), -1. / 8.},
-               {indexer->Nei(info, ix_c, iy_c, 1), 1. / 2.},
-               {This(info, ix_c, iy_c), -3. / 8.}}};
-    return {{{indexer->Nei(info, ix_c, iy_c, -1), -1. / 8.},
-             {indexer->Nei(info, ix_c, iy_c, 1), 1. / 8.},
-             {This(info, ix_c, iy_c), 0.}}};
+      return {{{indexer->Nei(info_c, ix_c, iy_c, 2), -1. / 8.},
+               {indexer->Nei(info_c, ix_c, iy_c, 1), 1. / 2.},
+               {This(info_c, ix_c, iy_c), -3. / 8.}}};
+    return {{{indexer->Nei(info_c, ix_c, iy_c, -1), -1. / 8.},
+             {indexer->Nei(info_c, ix_c, iy_c, 1), 1. / 8.},
+             {This(info_c, ix_c, iy_c), 0.}}};
   }
   std::array<std::pair<long long, double>, 3> static D2(
       const Info *info, const EdgeCellIndexer *indexer, int ix, int iy) {
