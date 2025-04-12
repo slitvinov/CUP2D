29cf38351e176ecdd993c6910c6f6a4bc281226f
Sergey Litvinov <slitvinov@gmail.com>
2025-04-12 12:16:16 +0200

    inline D2

--- main.cpp
+++ main.cpp
@@ -1999,20 +1999,6 @@ struct Solver {
   YminIndexer YminCell;
   YmaxIndexer YmaxCell;
   std::array<const EdgeCellIndexer *, 4> edgeIndexers;
-  std::array<std::pair<long long, double>, 3> static D2(
-      const Info *info_c, const EdgeCellIndexer *indexer, int ix_c, int iy_c) {
-    if (indexer->isBD(ix_c, iy_c))
-      return {{{indexer->Nei(info_c, ix_c, iy_c, -2), 1. / 32.},
-               {indexer->Nei(info_c, ix_c, iy_c, -1), -1. / 16.},
-               {This(info_c, ix_c, iy_c), 1. / 32.}}};
-    else if (indexer->isFD(ix_c, iy_c))
-      return {{{indexer->Nei(info_c, ix_c, iy_c, 2), 1. / 32.},
-               {indexer->Nei(info_c, ix_c, iy_c, 1), -1. / 16.},
-               {This(info_c, ix_c, iy_c), 1. / 32.}}};
-    return {{{indexer->Nei(info_c, ix_c, iy_c, -1), 1. / 32.},
-             {indexer->Nei(info_c, ix_c, iy_c, 1), 1. / 32.},
-             {This(info_c, ix_c, iy_c), -1. / 16.}}};
-  }
   void interpolate(const Info *info_c, int ix_c, int iy_c, const Info *info_f,
                    long long fine_close_idx, long long fine_far_idx,
                    double signInt, double signTaylor,
@@ -2037,7 +2023,18 @@ struct Solver {
              {This(info_c, ix_c, iy_c), 0.}}};
     for (int i = 0; i < 3; i++)
       row.mapColVal(rank_c, D[i].first, signTaylor * tf * D[i].second);
-    D = D2(info_c, indexer, ix_c, iy_c);
+
+    if (indexer->isBD(ix_c, iy_c))
+      D = {{{indexer->Nei(info_c, ix_c, iy_c, -2), 1. / 32.},
+               {indexer->Nei(info_c, ix_c, iy_c, -1), -1. / 16.},
+               {This(info_c, ix_c, iy_c), 1. / 32.}}};
+    else if (indexer->isFD(ix_c, iy_c))
+      D = {{{indexer->Nei(info_c, ix_c, iy_c, 2), 1. / 32.},
+               {indexer->Nei(info_c, ix_c, iy_c, 1), -1. / 16.},
+               {This(info_c, ix_c, iy_c), 1. / 32.}}};
+    D = {{{indexer->Nei(info_c, ix_c, iy_c, -1), 1. / 32.},
+             {indexer->Nei(info_c, ix_c, iy_c, 1), 1. / 32.},
+             {This(info_c, ix_c, iy_c), -1. / 16.}}};
     for (int i = 0; i < 3; i++)
       row.mapColVal(rank_c, D[i].first, tf * D[i].second);
   }
