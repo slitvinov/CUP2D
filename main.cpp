987ab3ee1cbc64254bdbef8b5c7171bfec83cedf
Sergey Litvinov <slitvinov@gmail.com>
2025-04-14 01:23:53 +0200

    critical

--- main.cpp
+++ main.cpp
@@ -1429,6 +1429,7 @@ static int adapt() {
                              sim.infos[j]->Znei[1 + x][1 + y]] ==
                     ChildrenAreActive) {
                   if (state[j] == Compress)
+#pragma omp critical
                     state[j] = Leave;
                   int Bstep = abs(x) + abs(y) == 2 ? 3 : 1;
                   for (int B = 0; B <= 1; B += Bstep) {
@@ -1440,6 +1441,7 @@ static int adapt() {
                     long long zzz = forward(m + 1, iNei, jNei);
                     int id = sim.levels[m + 1] + zzz;
                     if (state[sim.map[id]] == Refine) {
+#pragma omp critical
                       state[j] = Refine;
                       goto found;
                     }
@@ -1474,6 +1476,7 @@ static int adapt() {
                    sim.infos[j]->Znei[1 + cx][1 + cy];
           if (sim.map.find(id) != sim.map.end() &&
               state[sim.map[id]] == Refine) {
+#pragma omp critical
             state[j] = Leave;
             break;
           }
