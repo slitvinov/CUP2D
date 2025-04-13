d58983227d0679e9d6972a1d74751ccc31975bb8
Sergey Litvinov <slitvinov@gmail.com>
2025-04-14 01:19:49 +0200

    up

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 ${main=./main} \
 -AdaptSteps 20 \
 -CFL 0.5 \
--Ctol 3 \
+-Ctol 1 \
 -lambda 1e7 \
 -levelMax 6 \
 -levelStart 4 \
@@ -18,8 +18,3 @@ ${main=./main} \
    scale=0.125 orientation=0 omega=-0.3 xcenter=0.3 ycenter=0.5 sdf=box.raw
    scale=0.125 orientation=0 omega=0.3 xcenter=0.6 ycenter=0.5 sdf=box.raw
 '
-
-# -shapes '
-#   scale=0.75 orientation=0 omega=-0.05 xcenter=0.35 ycenter=0.55 sdf=sdf.raw
-#   scale=0.75 orientation=0 omega=-0.05 xcenter=0.65 ycenter=0.44 sdf=sdf.raw
-# '
