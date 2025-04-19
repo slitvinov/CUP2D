635d6bcefdced67850323e6e1ef7d03e286fa211
Sergey Litvinov <slitvinov@gmail.com>
2025-04-19 19:58:49 +0200

    up

--- main.cpp
+++ main.cpp
@@ -182,7 +182,7 @@ static void get_states(Info *info, TreeState nei[3][3]) {
     if (cx == 0 && cy == 0)
       continue;
     long long id = sim.levels[info->level] + info->Znei[1 + cx][1 + cy];
-    nei[1 + cx][1 + cy] = sim.tree[id];
+    nei[1 + cx][1 + cy] = sim.tree.at(id);
     if (sim.tree[id] == Active)
       assert(0 <= sim.map.at(id) && sim.map.at(id) < sim.n);
   }
