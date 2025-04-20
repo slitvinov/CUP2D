be2f62841805d6c02f106c208281a18196825dc4
Sergey Litvinov <slitvinov@gmail.com>
2025-04-20 13:54:51 +0200

    copy

--- main.cpp
+++ main.cpp
@@ -245,8 +245,8 @@ public:
     sfc_inverse(info->Z, info->level, &xi, &yi);
     assert(m != NULL);
     Real *p = p0;
-    for (int iy = -(-ss); iy < -(-ss) + BS; iy += 4) {
-      Real *q = m + dim * iy * nm - dim * (-ss);
+    for (int iy = ss; iy < ss + BS; iy += 4) {
+      Real *q = m + dim * iy * nm + dim * ss;
       memcpy(q, p, sizeof(Real) * dim * BS), q += dim * nm, p += dim * BS;
       memcpy(q, p, sizeof(Real) * dim * BS), q += dim * nm, p += dim * BS;
       memcpy(q, p, sizeof(Real) * dim * BS), q += dim * nm, p += dim * BS;
