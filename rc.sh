d74ef7f2e29fb819968c1e2ece0543277f713c2b
Sergey Litvinov <slitvinov@seas.harvard.edu>
2026-03-29 17:34:51 -0400

    -O3

--- rc.sh
+++ rc.sh
@@ -8,7 +8,7 @@ rm -rf "${d?not set}" &&
    git checkout '${1-HEAD}' &&
    module load gcc/13 openmpi cuda python &&
    python3 gen_table.py &&
-   make -j "CXXFLAGS = -coverage -Og -g3" "LDFLAGS = -Xcompiler -coverage" &&
+   make -j "CXXFLAGS = -coverage -O3 -g3" "LDFLAGS = -Xcompiler -coverage" &&
    OMP_NUM_THREADS=4 srun --mpi=pmix -p gpu_test -c 1 -n 2 -N 1 -G 1 --mem 2Gb -t 30 sh -x run.sh &&
    source activate jepa
    # ls vel.*.xdmf2 | xargs -n 1 -P `nproc --all` tool/post.py &&
