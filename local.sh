4586eeefd55bd6ef0afdb369842de1e1b6a8b337
Sergey Litvinov <slitvinov@seas.harvard.edu>
2026-03-30 03:20:47 -0400

    local

---
+++ local.sh
@@ -0,0 +1,8 @@
+python3 gen_table.py &&
+make CPU=1 clean &&
+make CPU=1 -j CC=clang CXX=clang++ \
+  "OPENMPFLAGS=-Xpreprocessor -fopenmp" \
+  "CFLAGS=-O2 -g -I/opt/homebrew/opt/libomp/include" \
+  "CXXFLAGS=-O2 -g -I/opt/homebrew/opt/libomp/include" \
+  "LDFLAGS=-L/opt/homebrew/opt/libomp/lib -lomp" &&
+OMP_NUM_THREADS=4 sh run.sh
