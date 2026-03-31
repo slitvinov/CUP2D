f6fd64e9368f2fafc36757058096bf0efa5b10dc
Sergey Litvinov <slitvinov@seas.harvard.edu>
2026-03-30 20:14:54 -0400

    save old post.py

--- tool/post.py
+++ tool/post.py
@@ -8,17 +8,14 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.patches
-import statistics
 
 plt.rcParams['image.cmap'] = 'RdBu'
 for path in sys.argv[1:]:
     path = re.sub("[.]xdmf2$", "", path)
-    path = re.sub("[.]chi[.]raw$", "", path)
     path = re.sub("[.]xyz[.]raw$", "", path)
     png_path = path + ".png"
     xdmf_path = path + ".xdmf2"
     xyz_path = path + ".xyz.raw"
-    chi_path = path + ".chi.raw"
     vel_path = path + ".vel.raw"
     tmp_path = path + ".tmp.raw"
     if not os.path.isfile(png_path):
@@ -28,20 +25,18 @@ for path in sys.argv[1:]:
         xyz = np.memmap(xyz_path, "float32", "r")
         xyz = xyz.reshape(-1, 4, 2)
         ncell = len(xyz)
-        chi = np.memmap(chi_path, "float64", "r")
         tmp = np.memmap(tmp_path, "float64", "r")
         vel = np.memmap(vel_path, "float64", "r")
         vel = vel.reshape(-1, 2)
         patches = []
         color = []
         for i in range(ncell):
-            if chi[i] < 0.5:
-                x = xyz[i, 0, 0]
-                y = xyz[i, 0, 1]
-                lx = xyz[i, 2, 0] - x
-                ly = xyz[i, 2, 1] - y
-                color.append(tmp[i])
-                patches.append(matplotlib.patches.Rectangle((x, y), lx, ly))
+            x = xyz[i, 0, 0]
+            y = xyz[i, 0, 1]
+            lx = xyz[i, 2, 0] - x
+            ly = xyz[i, 2, 1] - y
+            color.append(tmp[i])
+            patches.append(matplotlib.patches.Rectangle((x, y), lx, ly))
         p = matplotlib.collections.PatchCollection(patches,
                                                    edgecolor='black',
                                                    linewidth=0.1)
